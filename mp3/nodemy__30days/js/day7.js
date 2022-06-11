59e4f1151d2e1a8106eb64a8ac0267bbbc4b32f6
{huy} <{Huybmt2527000@gmail.com}>
2022-06-12 00:31:24 +0700

    upload

--- mp3/nodemy__30days/js/day7.js
+++ mp3/nodemy__30days/js/day7.js
@@ -2,57 +2,96 @@ const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
 const inputElement = $('.input')
-const spanElement = $('.content__input-span')
 const buttonElement = $('.button')
-const close = $('.icon')
 const removeall = $('.delete')
+const save = $('.content__input-save')
 
+const app = {
+    folder: [
+        {
+            name : 'Reacjs'
+        },
+        {
+            name : 'Javascript'
+        },
+        {
+            name : 'Reacjs'
+        },
+        {
+            name : 'Javascript'
+        },
+    ],
+    
+    handelEvent: function() {
+       
+    },
 
+    start: function() {
+        app.render()
+        app.handelEvent()
+    },
 
-
-// document.addEventListener('keydown', function(e) {
-//     if(e.which == 13) {
-//         spanElement.innerText = (inputElement.value)
-//     }
-// })
-
-
-
-function toast({title="", message="", animation=""}) {
-    const main = document.querySelector('.content__input')
-    if(main) {
-        console.log(main)
-        const toast = document.createElement('div')
-        toast.classList.add('content__input-save');
-        // toast.style.animation = `Easin ease-in 1s`;
-         toast.innerHTML = `
-         <div class="push">
-            <span class="content__input-span">ReacJs</span>
-            <i class="fa-solid fa-xmark icon"></i>
-        </div>
-        <div class="push">
-            <span class="content__input-span">PHP</span>
-            <i class="fa-solid fa-xmark icon"></i>
-        </div>
-         `;
-         main.appendChild(toast); 
+    render: function() {
+        const main = document.querySelector('.content__input')
+        if(main) {
+            console.log(main)
+            var test = $('.content__input-save')
+            // toast.style.animation = `Easin ease-in 1s`;
+            const htmls = this.folder.map(function(course, index) {
+                return `
+                <div class="push">
+                   <span class="content__input-span">${course.name}</span>
+                   <i class="fa-solid fa-xmark icon" data-index="${index}"></i>
+               </div>
+                `;
+    
+            })
+            test.innerHTML = htmls.join('')
+            //  main.appendChild(toast);    
+        }
     }
 }
-function show(){
-    toast({
-    title : 'Hoi lam gi',
-    message: 'notify',
-    animation: 500,
-});
+
+app.start()
+const push = $('.push')
+
+// them node
+function demo() {
+
+        const node = document.createElement("div");
+        node.classList.add('push');
+        const textnode = document.createTextNode(inputElement.value)
+        const span = document.createElement("span");
+        span.classList.add('content__input-span')
+        span.appendChild(textnode);
+        node.appendChild(span);
+        save.appendChild(node);
+        
 }
 
-show()
+document.addEventListener('keydown', function(e) {
+        if(e.which == 13) {
+            demo()
+        }
+})
 
-buttonElement.onclick = function(e) {
-    const push = $('.push')
-    const test = $('.content__input-save')
-    push.remove()
-    if(!push) {
-        e.preventDefault()
+// nhan vao close
+const close = $$('.icon')
+console.log(close)
+close.forEach(function(course) {
+    course.onclick = function(e) {
+        if(push) {
+            save.removeChild(save.firstElementChild)
+            console.log([push])
+        }
     }
+    
+})
+
+// remove all
+buttonElement.onclick = function() {
+    const pushall = $$('.push')
+    pushall.forEach(function(course) {
+        course.remove()
+    })
 }
